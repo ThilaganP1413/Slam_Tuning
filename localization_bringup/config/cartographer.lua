7eaa7c957af57452c4f855bc1efa2e37eb07a6a0
ThilaganP1413 <thilaganp1413@google.com>
2026-05-19 18:45:49 +0530

    Param_15_cart

--- localization_bringup/config/cartographer.lua
+++ localization_bringup/config/cartographer.lua
@@ -1,17 +1,3 @@
--- Copyright 2016 The Cartographer Authors
---
--- Licensed under the Apache License, Version 2.0 (the "License");
--- you may not use this file except in compliance with the License.
--- You may obtain a copy of the License at
---
---      http://www.apache.org/licenses/LICENSE-2.0
---
--- Unless required by applicable law or agreed to in writing, software
--- distributed under the License is distributed on an "AS IS" BASIS,
--- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
--- See the License for the specific language governing permissions and
--- limitations under the License.
-
 include "map_builder.lua"
 include "trajectory_builder.lua"
 
@@ -44,69 +30,74 @@ options = {
 
 MAP_BUILDER.use_trajectory_builder_2d = true
 MAP_BUILDER.num_background_threads = 16
-TRAJECTORY_BUILDER_2D.num_accumulated_range_data = 1 -- no of lidar
+TRAJECTORY_BUILDER_2D.num_accumulated_range_data = 1 -- no of parts a full 360 degree scan is divided into
+
+TRAJECTORY_BUILDER_2D.min_range = 0.5
+TRAJECTORY_BUILDER_2D.max_range = 17.0
+TRAJECTORY_BUILDER_2D.missing_data_ray_length = 5.0
+
+-- LOCAL SLAM
+
+TRAJECTORY_BUILDER_2D.motion_filter.max_time_seconds = 1.0
+TRAJECTORY_BUILDER_2D.motion_filter.max_distance_meters = 0.5
+TRAJECTORY_BUILDER_2D.motion_filter.max_angle_radians = 0.2
 
 TRAJECTORY_BUILDER_2D.submaps.num_range_data = 90
-TRAJECTORY_BUILDER_2D.min_range = 0.05
-TRAJECTORY_BUILDER_2D.max_range = 20
-TRAJECTORY_BUILDER_2D.missing_data_ray_length = 5.0
+
 TRAJECTORY_BUILDER_2D.use_imu_data = false
+
 TRAJECTORY_BUILDER_2D.use_online_correlative_scan_matching = false
 TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.linear_search_window = 0.01
 TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.angular_search_window = math.rad(1.)
 TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.translation_delta_cost_weight = 30.
 TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.rotation_delta_cost_weight = 1e-1
 
-TRAJECTORY_BUILDER_2D.ceres_scan_matcher.translation_weight = 50
-TRAJECTORY_BUILDER_2D.ceres_scan_matcher.rotation_weight = 120
+TRAJECTORY_BUILDER_2D.ceres_scan_matcher.occupied_space_weight = 10
+TRAJECTORY_BUILDER_2D.ceres_scan_matcher.translation_weight = 500
+TRAJECTORY_BUILDER_2D.ceres_scan_matcher.rotation_weight = 500
 
---new start
-TRAJECTORY_BUILDER_2D.ceres_scan_matcher.ceres_solver_options.use_nonmonotonic_steps = true
-TRAJECTORY_BUILDER_2D.ceres_scan_matcher.ceres_solver_options.max_num_iterations = 20
+TRAJECTORY_BUILDER_2D.ceres_scan_matcher.ceres_solver_options.use_nonmonotonic_steps = false
+TRAJECTORY_BUILDER_2D.ceres_scan_matcher.ceres_solver_options.max_num_iterations = 15
 TRAJECTORY_BUILDER_2D.ceres_scan_matcher.ceres_solver_options.num_threads = 8
---new end
 
+TRAJECTORY_BUILDER_2D.submaps.grid_options_2d.grid_type = "PROBABILITY_GRID"
 TRAJECTORY_BUILDER_2D.submaps.range_data_inserter.probability_grid_range_data_inserter.hit_probability = 0.55
 TRAJECTORY_BUILDER_2D.submaps.range_data_inserter.probability_grid_range_data_inserter.miss_probability = 0.49
 
---new start
 TRAJECTORY_BUILDER_2D.submaps.grid_options_2d.resolution = 0.05
---new end
 
-TRAJECTORY_BUILDER_2D.voxel_filter_size = 0.25
+-- GLOBAL SLAM
 
---new start
-TRAJECTORY_BUILDER_2D.adaptive_voxel_filter.max_length = 0.5
-TRAJECTORY_BUILDER_2D.adaptive_voxel_filter.min_num_points = 50
---new end
+POSE_GRAPH.optimize_every_n_nodes = 100
+POSE_GRAPH.constraint_builder.log_matches = false -- DEBUG LOGGING
+POSE_GRAPH.log_residual_histograms = false -- DEBUG LOGGING
 
-POSE_GRAPH.optimization_problem.huber_scale = 2e2
-POSE_GRAPH.optimize_every_n_nodes = 80
-POSE_GRAPH.constraint_builder.min_score = 0.72
+POSE_GRAPH.constraint_builder.max_constraint_distance = 20 -- Loop Closure Maximum Distance
+POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.linear_search_window = 15.0
+POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.angular_search_window = math.rad(30.)
 
 POSE_GRAPH.constraint_builder.sampling_ratio = 0.3
-POSE_GRAPH.constraint_builder.log_matches = false
-
-POSE_GRAPH.global_constraint_search_after_n_seconds = 30
-POSE_GRAPH.constraint_builder.max_constraint_distance = 10
 
 POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.branch_and_bound_depth = 6
 
---new
-POSE_GRAPH.constraint_builder.ceres_scan_matcher.translation_weight = 5
-POSE_GRAPH.constraint_builder.ceres_scan_matcher.rotation_weight = 50
---new
+POSE_GRAPH.optimization_problem.huber_scale = 50
+POSE_GRAPH.constraint_builder.min_score = 0.35
 
-POSE_GRAPH.constraint_builder.loop_closure_translation_weight = 1e3
-POSE_GRAPH.constraint_builder.loop_closure_rotation_weight = 1e3
+POSE_GRAPH.global_constraint_search_after_n_seconds = 60
 
-POSE_GRAPH.matcher_translation_weight = 300
-POSE_GRAPH.matcher_rotation_weight = 2500
-POSE_GRAPH.max_num_final_iterations = 2000
+POSE_GRAPH.constraint_builder.ceres_scan_matcher.translation_weight = 120
+POSE_GRAPH.constraint_builder.ceres_scan_matcher.rotation_weight = 90
 
-POSE_GRAPH.optimization_problem.local_slam_pose_translation_weight = 80
+POSE_GRAPH.constraint_builder.loop_closure_translation_weight = 1000
+POSE_GRAPH.constraint_builder.loop_closure_rotation_weight = 1000
+
+POSE_GRAPH.matcher_translation_weight = 60
+POSE_GRAPH.matcher_rotation_weight = 80
+POSE_GRAPH.max_num_final_iterations = 1000
+
+POSE_GRAPH.optimization_problem.local_slam_pose_translation_weight = 150
 POSE_GRAPH.optimization_problem.local_slam_pose_rotation_weight = 100
-POSE_GRAPH.optimization_problem.odometry_translation_weight = 800
-POSE_GRAPH.optimization_problem.odometry_rotation_weight = 1200
+POSE_GRAPH.optimization_problem.odometry_translation_weight = 400
+POSE_GRAPH.optimization_problem.odometry_rotation_weight = 450
 
 return options
